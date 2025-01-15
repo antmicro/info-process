41f62d34407f8fb33127306fca412787dd23e6c1
Jan Malek <jmalek@antmicro.com>
2025-01-15 14:24:00 +0100

    [#71608] info-process: Implement adding two-way toggle branch entries

--- info-process.py
+++ info-process.py
@@ -4,12 +4,21 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import argparse
-from parser import Stream
+from parser import Stream, Record
+
+def two_way_toggle_handler(params: str, file: Record) -> tuple[str]:
+    # <LINE NUMBER>,<BLOCK>,<NAME>,<HIT COUNT>
+    line_number, block, name, hit_count = params.split(',', 4)
+    return (
+        f'{line_number},{block},{name}_0->1,{hit_count}',
+        f'{line_number},{block},{name}_1->0,{hit_count}',
+    )
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("input", type=str)
     parser.add_argument("--output", type=str)
+    parser.add_argument('--add-two-way-toggles', action='store_true', default=False)
     args = parser.parse_args()
 
     # Default to a in-place modification if no output path is specified
@@ -17,6 +26,9 @@ def main():
         args.output = args.input
 
     stream = Stream()
+    if args.add_two_way_toggles:
+        stream.install_handler("BRDA", two_way_toggle_handler)
+
     with open(args.input, "rt") as f:
         stream.run(f)
 
