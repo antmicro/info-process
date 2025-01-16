231a31b86421a35f95bac34ffc548aee94dba471
Jan Malek <jmalek@antmicro.com>
2025-01-16 10:11:35 +0100

    [#71608] info-process: Implement record filtering

--- info-process.py
+++ info-process.py
@@ -4,7 +4,8 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import argparse
-from parser import Stream, Record
+from parser import Stream, Record, EntryHandler, RemoveRecord
+import re
 
 def two_way_toggle_handler(params: str, file: Record) -> tuple[str, str]:
     # <LINE NUMBER>,<BLOCK>,<NAME>,<HIT COUNT>
@@ -21,6 +22,15 @@ def missing_brda_handler(params: str, file: Record) -> str:
         file.add('BRDA', f'{line_number},0,toggle,{hit_count}')
     return params
 
+def create_filter_handler(pattern: str) -> EntryHandler:
+    regex = re.compile(pattern)
+    def handler(path: str, file: Record) -> str:
+        if regex.search(path):
+            return path
+        raise RemoveRecord()
+
+    return handler
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('input', type=str,
@@ -32,6 +42,8 @@ def main():
     parser.add_argument('--add-missing-brda-entries', action='store_true', default=False,
                         help='Generate BRDA entries for lines which only have DA entries. ' +
                         'Can be combined with --add-two-way-toggles to get separate entries for each added toggle')
+    parser.add_argument('--filter', type=str,
+                        help='Only keep entries for files matching the provided regular expression')
     args = parser.parse_args()
 
     # Default to a in-place modification if no output path is specified
@@ -39,6 +51,10 @@ def main():
         args.output = args.input
 
     stream = Stream()
+
+    if args.filter is not None:
+        stream.install_handler('SF', create_filter_handler(args.filter))
+
     if args.add_two_way_toggles:
         stream.install_handler('BRDA', two_way_toggle_handler)
 
