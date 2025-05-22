533b18fbec033f527ffa40f340e37fceba54415d
Jan Malek <jmalek@antmicro.com>
2025-05-22 08:45:19 +0200

    [#76820] waive.py: Allow excluding group ranges

--- info_process/waive.py
+++ info_process/waive.py
@@ -4,20 +4,21 @@
 from dataclasses import dataclass
 from pathlib import Path
 import argparse
-from collections import defaultdict
-from .parser import Stream, Record, CategoryHandler, split_da, split_brda
+from .parser import Stream, Record, split_da, split_brda
 from . import handlers
-from typing import Optional
 import csv
 
 
 class ExplicitWaivers:
     LINE_NUMBER_WHOLE_FILE_MARKER: int = 0
+    GROUP_WHOLE_LINE_MARKER: int = 0
 
     @dataclass(frozen=True)
     class Exclude:
         line_start: int # the first line of the range to exclude inclusive, zero means to exclude the whole file
         line_end: int # the last line of the range to exclude inclusive, line_start == line_end means exclude a single line, line_start == line_end == 0 means exclude whole file
+        group_start: int
+        group_end: int
 
     def __init__(self, path: Path = None) -> None:
         self.excluded: dict[str, list[ExplicitWaivers.Exclude]] = {}
@@ -26,22 +27,39 @@ class ExplicitWaivers:
             # For such case is_excluded always returns False, so no exclusion at all
             return
 
+        def get_or_default(l: list[str], index: int, min_len: int) -> int:
+            assert min_len > index, f'{min_len=} has to be greater than {index=}'
+            if len(l) >= min_len:
+                return int(l[index])
+            return 0
+
         with open(path, newline='') as f:
             reader = csv.reader(f)
             for row in reader:
                 file = row[0]
                 if file not in self.excluded:
                     self.excluded[file] = list()
-                self.excluded[file].append(ExplicitWaivers.Exclude(int(row[1]), int(row[2])))
+                self.excluded[file].append(ExplicitWaivers.Exclude(
+                    line_start=get_or_default(row, 1, 3),
+                    line_end=get_or_default(row, 2, 3),
+                    group_start=get_or_default(row, 3, 5),
+                    group_end=get_or_default(row, 4, 5),
+                ))
 
-    def is_excluded(self, file: str, line_number: int) -> bool:
+    def is_excluded(self, file: str, line_number: int, group_number: int=-1) -> bool:
         for path, blacklist in self.excluded.items():
             if not file == path:
                 # Exclusion list doesn't impact the file
                 continue
 
             for entry in blacklist:
-                if (entry.line_start == entry.line_end == self.LINE_NUMBER_WHOLE_FILE_MARKER) or (entry.line_start <= line_number and line_number <= entry.line_end ):
+                line_excluded = (entry.line_start == entry.line_end == self.LINE_NUMBER_WHOLE_FILE_MARKER) or \
+                    (entry.line_start <= line_number <= entry.line_end)
+
+                group_excluded = group_number < 0 or (entry.group_start == entry.group_end == self.GROUP_WHOLE_LINE_MARKER) or \
+                    (entry.group_start <= group_number <= entry.group_end)
+
+                if line_excluded and group_excluded:
                     # Exclusion entry matched
                     return True
 
@@ -52,10 +70,11 @@ def create_waivers_handler(waivers: ExplicitWaivers):
         passed = []
         for entry in data:
             if prefix == 'BRDA':
-                line_number, _, __, ___ = split_brda(entry)
+                line, group, _, __ = split_brda(entry)
             elif prefix == 'DA':
-                line_number, _ = split_da(entry)
-            if not waivers.is_excluded(file.source_file, line_number):
+                line, _ = split_da(entry)
+                group = -1
+            if not waivers.is_excluded(file.source_file, line, group):
                 passed.append(entry)
         return passed
 
